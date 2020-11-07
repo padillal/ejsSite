f5c04e0e29ed93228efb40c1e2d49decacb907f1
padillal <padillal48@yahoo.com>
2020-11-07 15:35:38 -0800

    added form and place holders

--- assignment/routes/index.js
+++ assignment/routes/index.js
@@ -1,9 +1,27 @@
 var express = require('express');
 var router = express.Router();
 
+const content = [
+   {
+     text: "Hi there!",
+     user: "Amando",
+     added: new Date()
+   },
+   {
+     text: "Hello World!",
+     user: "Charles",
+     added: new Date()
+   }
+];
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { messages: content , title: 'PROJECT HOME PAGE', header: 'CONTENT LIST' });
+});
+
+/*GET new page*/
+router.get('/new', function(req, res, next) {
+  res.render('form');
 });
 
 module.exports = router;
